e47aede8db709b2540ca327be473445224982ce4
Babu Goud Chekkilla <rebelbob11@Rebelbobs-MBP.home>
2018-04-28 22:23:59 -0400

    done

--- server.js
+++ server.js
@@ -3,37 +3,67 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
+mongoose.Promise = Promise
 
+var dbUrl = 'mongodb://user:user@ds161529.mlab.com:61529/db_chatapp'
 
-
-var messages = [
-    { name: 'Tim', message: 'Hi' },
-    { name: 'Jane', message: 'Hello' }
-]
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
-app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    res.sendStatus(200)
-    io.emit('message', req.body)
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({ name: user }, (err, messages) => {
+        res.send(messages)
+    })
 })
+
+app.post('/messages', async(req, res) => {
+
+    try {
+        var message = new Message(req.body)
+
+        var savedMessage = await message.save()
+
+        console.log('saved')
+
+        var censored = await Message.findOne({ message: 'badword' })
+
+        if (censored)
+            await Message.remove({ _id: censored.id })
+        else
+            io.emit('message', req.body)
+
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    } finally {
+        console.log('message post called')
+    }
+})
+
+
 
 io.on('connection', (socket) => {
-    console.log("New user Connected");
+    console.log('a user connected')
+})
 
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
